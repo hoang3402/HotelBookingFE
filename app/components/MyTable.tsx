546770f6eea133cd07e69f33ae42885ead1089a8
HoangDSR <54090585+hoang3402@users.noreply.github.com>
2024-03-13 10:58:33 +0700

    Update dependencies for `@nextui-org/table`

---
+++ app/components/MyTable.tsx
@@ -0,0 +1,20 @@
+import {getKeyValue, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow} from "@nextui-org/table";
+
+const MyTable = ({columns, rows}: any) => {
+  return (
+    <Table aria-label="Example table with dynamic content">
+      <TableHeader columns={columns}>
+        {(column: any) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+      </TableHeader>
+      <TableBody items={rows}>
+        {(item: any) => (
+          <TableRow key={item.key}>
+            {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default MyTable;
