8aa44ef9a6f908116232b9f6b56dee17af5f7da7
HoangDSR <54090585+hoang3402@users.noreply.github.com>
2024-03-13 11:55:21 +0700

    Refactor: Upgrade NextUI to v2.2.10 and Framer Motion to v11.0.12, and implement tabs navigation for booking status

--- app/account/bookings/page.tsx
+++ app/account/bookings/page.tsx
@@ -6,16 +6,21 @@ import NextAuth from "@auth-kit/next";
 import {domain} from "@/app/actions/getRoomById";
 import useAuthHeader from "react-auth-kit/hooks/useAuthHeader";
 import Loader from "@/app/components/Loader";
-import Image from "next/image";
-import {FormattedDate, FormattedPrice} from "@/app/components/Ultility";
 import {useRouter} from "next/navigation";
+import {Tab, Tabs} from "@nextui-org/tabs";
+import Image from 'next/image'
 import Button from "@/app/components/Button";
+import {FormattedDate, FormattedPrice} from "@/app/components/Ultility";
 
 export default function Page() {
   const token = useAuthHeader()
   const [isLoading, setIsLoading] = useState(true)
   const [bookings, setBookings] = useState([])
   const route = useRouter()
+  const [activeTab, setActiveTab] = useState('upcoming');
+  const handleTabChange = (newTabKey: any) => setActiveTab(newTabKey);
+  const filteredBookings = (status: any) =>
+    bookings.filter((booking: any) => booking.status === status);
 
   useEffect(() => {
     fetch(`${domain}api/booking/`, {
@@ -38,37 +43,30 @@ export default function Page() {
           <div className="max-w-screen-lg mx-auto">
             <h1>You have {bookings.length} booking</h1>
             <br/>
-            <div className={'grid grid-cols-1 md:grid-cols-2 gap-4'}>
-              {bookings.map((booking: any) => (
-                <div key={booking.id} className={'w-full flex gap-4 border rounded-2xl p-4 hover:shadow-md transition'}>
-                  <div className={'w-4/5 h-full cursor-pointer'}
-                       onClick={() => {
-                         route.push('/hotel/' + booking.hotel.id)
-                       }}>
-                    <Image
-                      className={'size-full object-cover rounded-2xl hover:scale-105 transition'}
-                      src={booking.hotel.image}
-                      alt={booking.hotel.name}
-                      width={200} height={200}
-                    />
+            <div>
+              <Tabs onSelect={handleTabChange}>
+                <Tab key="upcoming" title="Upcoming">
+                  <div className={'grid grid-cols-1 md:grid-cols-2 gap-4'}>
+                    {filteredBookings('Pending').map((booking: any) => (
+                      <BookingCard key={booking.id} booking={booking} route={route}/>
+                    ))}
                   </div>
-                  <div className={'w-full overflow-hidden'}>
-                    <h1 className={'truncate text-xl'}>{booking.hotel.name}: {booking.room.name}</h1>
-                    <p>Price: {FormattedPrice(booking.total_price_usd, 'USD')}</p>
-                    <p>Status: {booking.status}</p>
-                    <p>Check in: {FormattedDate(booking.check_in_date)}</p>
-                    <p>Check out: {FormattedDate(booking.check_out_date)}</p>
-                    <div>
-                      <Button
-                        onClick={() => {
-                          route.push('/account/bookings/' + booking.id)
-                        }}
-                        label={'Details'}
-                      />
-                    </div>
+                </Tab>
+                <Tab key="completed" title="Completed">
+                  <div className={'grid grid-cols-1 md:grid-cols-2 gap-4'}>
+                    {filteredBookings('Completed').map((booking: any) => (
+                      <BookingCard key={booking.id} booking={booking} route={route}/>
+                    ))}
                   </div>
-                </div>
-              ))}
+                </Tab>
+                <Tab key="cancelled" title="Cancelled">
+                  <div className={'grid grid-cols-1 md:grid-cols-2 gap-4'}>
+                    {filteredBookings('Cancelled').map((booking: any) => (
+                      <BookingCard key={booking.id} booking={booking} route={route}/>
+                    ))}
+                  </div>
+                </Tab>
+              </Tabs>
             </div>
           </div>
         </Container>
@@ -76,3 +74,36 @@ export default function Page() {
     </NextAuth>
   )
 }
+
+const BookingCard = ({booking, route}: any) => {
+  return (
+    <div className={'w-full flex gap-4 border rounded-2xl p-4 hover:shadow-md transition'}>
+      <div key={booking.id} className={'w-4/5 h-full cursor-pointer'}
+           onClick={() => {
+             route.push('/hotel/' + booking.hotel.id)
+           }}>
+        <Image
+          className={'size-full object-cover rounded-2xl hover:scale-105 transition'}
+          src={booking.hotel.image}
+          alt={booking.hotel.name}
+          width={200} height={200}
+        />
+      </div>
+      <div className={'w-full overflow-hidden'}>
+        <h1 className={'truncate text-xl'}>{booking.hotel.name}: {booking.room.name}</h1>
+        <p>Price: {FormattedPrice(booking.total_price_usd, 'USD')}</p>
+        <p>Status: {booking.status}</p>
+        <p>Check in: {FormattedDate(booking.check_in_date)}</p>
+        <p>Check out: {FormattedDate(booking.check_out_date)}</p>
+        <div>
+          <Button
+            onClick={() => {
+              route.push('/account/bookings/' + booking.id)
+            }}
+            label={'Details'}
+          />
+        </div>
+      </div>
+    </div>
+  )
+};
