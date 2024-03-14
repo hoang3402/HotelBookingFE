d8e7c71f89df4f30fc0edfdf2b757efd8210cb06
HoangDSR <54090585+hoang3402@users.noreply.github.com>
2024-03-14 09:59:13 +0700

    Fix broken sidebar with improperly nested children

--- app/components/sidebar/SideBar.tsx
+++ app/components/sidebar/SideBar.tsx
@@ -20,26 +20,28 @@ export default function SideBar({children}: any) {
 
   return (
     <aside className="h-screen">
-      <nav className="h-full flex flex-col bg-white border-r shadow-sm">
-        <div className="p-4 pb-2 flex justify-between items-center">
-          <img
-            src="/images/logo.png"
-            className={`overflow-hidden transition-all ${
-              sideBar.isOpen ? "w-32" : "w-0"
-            }`}
-            alt=""
-            onClick={() => route.push('/')}
-          />
-          <button
-            onClick={sideBar.toggle}
-            className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100"
-          >
-            {sideBar.isOpen ? <LuChevronFirst/> : <LuChevronLast/>}
-          </button>
-        </div>
-
+      <nav className="h-full flex flex-col justify-between bg-white border-r shadow-sm">
         <div>
-          <ul className="flex-1 px-3">{children}</ul>
+          <div className="p-4 pb-2 flex justify-between items-center">
+            <img
+              src="/images/logo.png"
+              className={`overflow-hidden transition-all ${
+                sideBar.isOpen ? "w-32" : "w-0"
+              }`}
+              alt=""
+              onClick={() => route.push('/')}
+            />
+            <button
+              onClick={sideBar.toggle}
+              className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100"
+            >
+              {sideBar.isOpen ? <LuChevronFirst/> : <LuChevronLast/>}
+            </button>
+          </div>
+
+          <div>
+            <ul className="flex-1 px-3">{children}</ul>
+          </div>
         </div>
 
         {isClient ? (
