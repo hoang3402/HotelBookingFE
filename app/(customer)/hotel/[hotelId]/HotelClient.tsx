251dfd73a77ff045d5a1f9c5021c60da78b9e001
HoangDSR <54090585+hoang3402@users.noreply.github.com>
2024-03-18 10:06:22 +0700

    Enhances hotel listing view with read more, features, and styling

--- app/(customer)/hotel/[hotelId]/HotelClient.tsx
+++ app/(customer)/hotel/[hotelId]/HotelClient.tsx
@@ -5,10 +5,13 @@ import ListingHead from "@/app/components/listings/ListingHead";
 import Container from "@/app/components/Container";
 import {ListingRoom} from "@/app/components/listings/ListingRoom";
 import Loader from "@/app/components/Loader";
+import {MdDone} from "react-icons/md";
+import {Card} from "@nextui-org/card";
 
 const HotelClient = ({listing}: any) => {
 
   const [isClient, setIsClient] = useState(false)
+  const [isShow, setIsShow] = useState(false)
 
   useEffect(() => {
     setIsClient(true)
@@ -26,12 +29,32 @@ const HotelClient = ({listing}: any) => {
               id={listing?.id}
               currentUser={null}
             />
-            <div>
+
+
+            <Card className={'p-4'} >
               <h2 className="text-2xl font-bold">Description</h2>
-              <p className="text-lg">
-                {listing?.description}
-              </p>
-            </div>
+              {isShow ? (
+                <p>{listing?.description}</p>
+              ):(
+                <p className={'line-clamp-3'}>{listing?.description}</p>
+              )}
+              <div className={'cursor-pointer font-bold'} onClick={() => setIsShow(!isShow)}>
+                {isShow ? 'Show less' : 'Show more'}
+              </div>
+            </Card>
+
+            <Card className={'p-4'}>
+              <h2>Features</h2>
+              <div className={'grid'}>
+                {listing?.features?.map((feature: any) => (
+                  <div className={'flex items-center gap-2'}>
+                    <MdDone/>
+                    <p>{feature?.description}</p>
+                  </div>
+                ))}
+              </div>
+            </Card>
+
             <ListingRoom
               rooms={listing?.room_set} currency={listing?.province?.country?.currency}
             />
