b330a50b9d698867479ba7fb2ce9d794c46cbada
HoangDSR <54090585+hoang3402@users.noreply.github.com>
2024-03-18 16:43:59 +0700

    Done create review

--- app/(customer)/account/bookings/[id]/page.tsx
+++ app/(customer)/account/bookings/[id]/page.tsx
@@ -13,6 +13,11 @@ import {DateRange} from "react-date-range";
 import {FormattedDate, FormattedPrice} from "@/app/components/Ultility";
 import Button from "@/app/components/Button";
 import {toast} from "react-hot-toast";
+import {useRouter} from 'next/navigation';
+import {Card} from "@nextui-org/card";
+import {Tab, Tabs} from "@nextui-org/tabs";
+import {Textarea} from "@nextui-org/input";
+import ReactStars from 'react-stars'
 
 interface IParams {
   id?: string;
@@ -21,6 +26,10 @@ interface IParams {
 const BookingDetailPage = ({params}: { params: IParams }) => {
 
   const token = useAuthHeader()
+  const route = useRouter()
+  const [selected, setSelected] = React.useState("detail");
+  const [review, setReview] = useState("");
+  const [star, setStar] = useState(4);
   const [isLoading, setIsLoading] = useState(true)
   const [data, setData] = useState<any>({})
   const [selectedRange, setSelectedRange] = useState({
@@ -65,6 +74,29 @@ const BookingDetailPage = ({params}: { params: IParams }) => {
     })
   }
 
+  function handleBack() {
+    route.back()
+  }
+
+  function handleCreateReview() {
+    fetch(`${domain}api/comment/create/`,{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `${token}`
+      },
+      body: JSON.stringify({
+        "rating": star,
+        "title": `${params.id} - ${data.hotel.name} - ${data.room.name}`,
+        "comment": review,
+        "booking": params.id
+      })
+    }).then(() => {
+      toast.success("Create review success!")
+      route.back()
+    })
+  }
+
   return (
     <NextAuth fallbackPath={'/'}>
       {isLoading ? <Loader/> : (
@@ -72,20 +104,42 @@ const BookingDetailPage = ({params}: { params: IParams }) => {
           <div className={'flex flex-col gap-4'}>
             <h1 className={'text-2xl'}>{data.hotel.name} - {data.room.name}</h1>
 
-            <div className={'flex'}>
+            <div className={'flex gap-5'}>
               <div className={'flex w-1/2 flex-col gap-10'}>
-                <div className={'text-xl'}>
-                  <p>Price: {FormattedPrice(data.total_price_usd, 'USD')}</p>
-                  <p>Status: {data.status}</p>
-                  <p>Check in: {FormattedDate(data.check_in_date)}</p>
-                  <p>Check out: {FormattedDate(data.check_out_date)}</p>
-                  <p>Sleeps: {data.room.adults} adults {data.room.children ? `and ${data.room.children} children` : ""}</p>
-                  <p>Phone number: {data.hotel.phone_number}</p>
-                  <p>Email: {data.hotel.email}</p>
-                  <p>Created at: {FormattedDate(data.created_at)}</p>
-                </div>
-                <div className={'w-[200px]'}>
-                  <Button label={'Cancel'} onClick={handleCancel}/>
+                <Tabs className={'flex justify-center'}
+                      selectedKey={selected}
+                      onSelectionChange={(key: any) => setSelected(key)}
+                >
+                  <Tab key={'detail'} title={'Detail'}>
+                    <Card className={'p-4 text-xl'}>
+                      <p>Price: {FormattedPrice(data.total_price_usd, 'USD')}</p>
+                      <p>Status: {data.status}</p>
+                      <p>Check in: {FormattedDate(data.check_in_date)}</p>
+                      <p>Check out: {FormattedDate(data.check_out_date)}</p>
+                      <p>Sleeps: {data.room.adults} adults {data.room.children ? `and ${data.room.children} children` : ""}</p>
+                      <p>Phone number: {data.hotel.phone_number}</p>
+                      <p>Email: {data.hotel.email}</p>
+                      <p>Created at: {FormattedDate(data.created_at)}</p>
+                    </Card>
+                  </Tab>
+                  <Tab key={'Reviews'} title={'Reviews'}>
+                    <Card className={'p-4 text-xl'}>
+                      <ReactStars
+                        count={5}
+                        value={star}
+                        onChange={setStar}
+                        size={24}
+                        color2={'#ffd700'} />
+                      <Textarea value={review} onValueChange={setReview} label={'Leave a review'}/>
+                    </Card>
+                  </Tab>
+                </Tabs>
+
+                <div className={'flex gap-5'}>
+                  {selected === 'detail' ?
+                    <Button label={'Cancel'} onClick={handleCancel}/> :
+                    <Button label={'Send review'} onClick={handleCreateReview}/>}
+                  <Button label={'Back'} onClick={handleBack}/>
                 </div>
               </div>
               <div className={'w-1/2 border-2'}>
