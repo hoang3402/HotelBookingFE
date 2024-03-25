b80582353292550f81ed62e0ff7fefe8dea9101f
HoangDSR <nvh.02021995@gmail.com>
2024-03-25 09:58:11 +0700

    Add suspense boundary to enhance user experience

--- app/(customer)/page.tsx
+++ app/(customer)/page.tsx
@@ -1,15 +1,19 @@
 import Container from "@/app/components/Container";
 import HelpWidget from "@/app/components/chatbot/HelpWidget";
 import HomeClient from "@/app/(customer)/HomeClient";
+import {Suspense} from "react";
+import Loader from "@/app/components/Loader";
 
 
 const Home = () => {
 
   return (
     <Container>
-      <div className={'flex flex-col gap-5'}>
-        <HomeClient/>
-      </div>
+      <Suspense fallback={<Loader/>}>
+        <div className={'flex flex-col gap-5'}>
+          <HomeClient/>
+        </div>
+      </Suspense>
       <HelpWidget/>
     </Container>
   );
